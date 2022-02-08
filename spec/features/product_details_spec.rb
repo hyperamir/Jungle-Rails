ffc6c51df1ad62e04088f9d46dcbf000d04fcb13
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-02-08 16:04:20 -0500

    feature-spec product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product details" do
+    # ACT
+    visit root_path
+    find('img', match: :first).click
+    sleep 5
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'section.products-show'
+  end
+end
